76ddbe106005eebb9d71166aec80c1a221b1652c
Mikhail D <mikhail.dozmorov@gmail.com>
2014-04-22 10:52:31 -0600

    Simplify GWAScatalog processing into one gwas2bed.py script

--- README.md
+++ README.md
@@ -3,15 +3,28 @@ Extracting disease-specific genomic coordinates from GWAS catalog
 
 A collection of scripts to download and extract genomic coordinates of disease-, trait-, or phenotype-specific sets of SNPs from the [GWAS catalog](http://www.genome.gov/gwastudies/).
 
-
 * `gwas2bed.py` - A Pyhton script to download the coordinates and separate them into disease-specific .bed files
 
-* `renameSpecial.sh` - A script to remove spaces and special characters from file names
-
-* `gwassort.sh` - A script to sort large/small SNP sets into separate folders
-
-* `more15` - Files with disease- and trait-specific sets of SNPs, each set has at least 15 SNPs. Accessed 01/31/2014
-
-* `less15` - Files with disease- and trait-specific sets of SNPs, each set has less than 15 SNPs. Accessed 01/31/2014
-
-* `gwascatalog.bed` - coordinates of all GWAS catalog SNPs. Accessed 01/31/2014. 14,182 SNPs total.
+## ==================================================================
+## Get GWAScatalog data from the UCSC MySQL server, 
+## extract hg19 genomic coordinates into separate files 
+## defined by 'title' column
+##
+## Sort disease-associated SNP sets by size. If a disease has 15 SNPs
+## or less, it is not recommended to run enrichment analysis on it,
+## and the file is coplied into 'less15' folder, otherwise, into 'more15'
+## folder (Trynka et al. Chromatin marks identify critical cell types 
+## for fine mapping complex trait variants. Nat Genet. 2013 Feb;45(2):
+## 124-30. doi: 10.1038/ng.2504).
+##
+## Remove special characters from file names
+##
+## Usage: python gwas2bed.py [output dir]
+##
+## Example: python gwas2bed.py gwascatalog
+##
+## Output: A [output dir] folder containing .BED file with genomic
+## coordinates of all SNPs from GWAScatalog, and two subfolders,
+## "less15" and "more15", each containing .BED files with coordinates of
+## less than or more than 15 trait-specific SNPs, respectively.
+## ==================================================================
--- gwas2bed.py
+++ gwas2bed.py
@@ -1,31 +1,61 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 ## ==================================================================
 ## Get GWAScatalog data from the UCSC MySQL server, 
-## extract hg19 genomic coordinates in separate files 
+## extract hg19 genomic coordinates into separate files 
 ## defined by 'title' column
+##
+## Sort disease-associated SNP sets by size. If a disease has 15 SNPs
+## or less, it is not recommended to run enrichment analysis on it,
+## and the file is coplied into 'less15' folder, otherwise, into 'more15'
+## folder (Trynka et al. Chromatin marks identify critical cell types 
+## for fine mapping complex trait variants. Nat Genet. 2013 Feb;45(2):
+## 124-30. doi: 10.1038/ng.2504).
+##
+## Remove special characters from file names
 ##
 ## Usage: python gwas2bed.py [output dir]
 ##
 ## Example: python gwas2bed.py gwascatalog
+##
+## Output: A [output dir] folder containing .BED file with genomic
+## coordinates of all SNPs from GWAScatalog, and two subfolders,
+## "less15" and "more15", each containing .BED files with coordinates of
+## less than or more than 15 trait-specific SNPs, respectively.
 ## ==================================================================
 
 import MySQLdb
 import sys
 import os
+import time
+from contextlib import closing
 
 OUTDIR = sys.argv[1]
 if not os.path.exists(OUTDIR):
-    os.mkdir(OUTDIR)
+	os.mkdir(OUTDIR)
+	os.mkdir(os.path.join(OUTDIR, "less15"))
+	os.mkdir(os.path.join(OUTDIR, "more15"))
 
 conn = MySQLdb.connect(host="genome-mysql.cse.ucsc.edu", user="genomep", passwd="password", db="hg19")
-c = conn.cursor()
-c.execute("select distinct (trait) from gwasCatalog")
-traits = [r[0] for r in c]
-
-for trait in traits:
-	# trait.replace("'","\\'")
-	print trait
-	c.execute("select chrom, chromStart, chromEnd, name  from gwasCatalog where trait = %s group by chrom, chromStart, chromEnd, name", [trait])
-	with open(os.path.join(OUTDIR, trait.replace("'","").replace(" ","_").replace("/","-")), "w") as h:
+with closing(conn.cursor()) as c:
+	c.execute("select distinct chrom, chromStart, chromEnd, name, trait from gwasCatalog group by trait")
+	h = open(os.path.join(OUTDIR, "gwasCatalog_" + time.strftime("%d-%m-%Y") + ".bed"), "w")
+	try:
 		for row in c:
 			h.write("\t".join(map(str, row)) + "\n")
+	finally:
+		h.close()
+
+	c.execute("select distinct (trait) from gwasCatalog")
+	traits = [r[0] for r in c]	
+	for trait in traits:
+		c.execute("select distinct chrom, chromStart, chromEnd, name  from gwasCatalog where trait = %s group by chrom, chromStart, chromEnd, name", [trait])
+		if len(map(str,c)) < 15:
+			OUTDIRBED = os.path.join(OUTDIR, "less15")
+			print("Less: " + trait)
+		else:
+			OUTDIRBED = os.path.join(OUTDIR, "more15")
+			print("More: " + trait)
+		with open(os.path.join(OUTDIRBED, trait.replace("'","").replace(" ","_").replace("/","-").replace("&","").replace(";","_")) + ".bed", "w") as h:
+			for row in c:
+				h.write("\t".join(map(str, row)) + "\n")
+conn.close()
