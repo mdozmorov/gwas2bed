5cdffda560d8a582f464669e836680349cbcf563
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2017-07-25 08:41:39 -0500

    minor script tweaks

--- gwasCatalog/data/db_DisGeNet/make.sh
+++ gwasCatalog/data/db_DisGeNet/make.sh
@@ -3,7 +3,7 @@ export LANG=C
 
 mkdir -p genes
 
-zcat < curated_gene_disease_associations.tsv.gz | sed '1d' | cut -f1,5 | tr ' ' '_' | tr -d '[{}(),!]' | tr -d "'" | tr -d '`' | tr '[A-Z]' '[a-z]' | sed 's/_-_/_/g' | tr '\/' '_' | tr '-' '\_' | awk 'BEGIN {OFS="\t"} {print $1 >> ("genes/"$2".txt"); close("genes/"$2".txt")}'
+zcat < curated_gene_disease_associations.tsv.gz | sed '1d' | cut -f1,5 | tr ' ' '_' | tr -d '[{}(),!]' | tr -d "'" | tr -d '`' | tr '[A-Z]' '[a-z]' | sed 's/_-_/_/g' | tr '\/' '_' | tr '-' '_' | awk 'BEGIN {OFS="\t"} {print $1 >> ("genes/"$2".txt"); close("genes/"$2".txt")}'
 
 for file in genes/*.txt; do echo $file; sort $file | uniq > tmp.txt && mv tmp.txt $file; done 
 
